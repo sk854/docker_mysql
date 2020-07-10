2a4b4b2761861c150a1415c71a9cbb6765ec5277
Sooah Kang <sk854@cornell.edu>
2020-07-10 16:53:11 +0900

    edit

--- models/index.js
+++ models/index.js
@@ -8,12 +8,11 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize({
+  dialect: 'mysql',
+  host: 'localhost',
+  port: 3306
+});
 
 fs
   .readdirSync(__dirname)
